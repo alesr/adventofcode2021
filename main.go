d6083ec1d39b420abd8393b0f2997406cf94a36a
Alessandro Resta <alessandro.resta@gmail.com>
2021-12-05 17:59:47 +0100

    Implement day 2 part 1

--- main.go
+++ main.go
@@ -7,11 +7,13 @@ import (
 	"log"
 
 	"github.com/alesr/adventofcode2021/day1"
+	"github.com/alesr/adventofcode2021/day2"
 )
 
 func main() {
 	day1P1Input := readFile("day1/data.in")
 	day1P2Input := *day1P1Input
+
 	day1P1Result, err := day1.Part1(day1P1Input)
 	if err != nil {
 		log.Fatalln("failed to process day 1 part 1:", err)
@@ -24,8 +26,13 @@ func main() {
 	}
 	fmt.Println("Day 1 part 2:", day1P2Result)
 
+	day2P1Input := readFile("day2/data.in")
 
-	fmt.Println("Sonar sweep result:", result)
+	day2P1Result, err := day2.Part1(day2P1Input)
+	if err != nil {
+		log.Fatalln("failed to process day 2 part 1:", err)
+	}
+	fmt.Println("Day 2 part 1:", day2P1Result)
 }
 
 func readFile(filename string) *bytes.Buffer {
